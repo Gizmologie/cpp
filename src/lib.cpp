61be9633e2fbb2d8b5f2ba0b4be8374185021b8e
Aurelien <aurelienguillemot@hotmail.fr>
2018-09-19 11:40:21 +0200

    creation de la librairie calcul

---
+++ src/lib.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "lib.h"
+
+int calcul::somme (int nombre_1, int nombre_2) {
+	return (nombre_1 + nombre_2);
+}
---
+++ src/lib.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class calcul 
+{
+	public:
+		calcul ();
+		calcul (int a, int b);
+		~calcul ();
+
+		int somme (int nombre_1, int nombre_2);
+private:
+
+};
