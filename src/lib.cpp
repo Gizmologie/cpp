31ddf0dbb79952f719ea5e612078f43fe9752819
Aurelien <aurelienguillemot@hotmail.fr>
2018-09-26 09:38:43 +0200

    ajout de la fonction produit et quotient à lib.h

--- src/lib.cpp
+++ src/lib.cpp
@@ -4,3 +4,12 @@
 int calcul::somme (int nombre_1, int nombre_2) {
 	return (nombre_1 + nombre_2);
 }
+
+int calcul::produit (int nombre_3, int nombre_4) {
+	return (nombre_3 * nombre_4);
+}
+
+float calcul::quotient (int nombre_5, int nombre_6) {
+	return (nombre_5 / nombre_6);
+}
+
--- src/lib.h
+++ src/lib.h
@@ -8,6 +8,8 @@ class calcul
 	//	~calcul ();
 
 		int somme (int nombre_1, int nombre_2);
+		int produit (int nombre_3, int nombre_4);
+		float quotient (int nombre_5, int nombre_6);
 private:
 
 };
